bfb399bd7e3fc5887464779066a809598030c73f
pagar22 <aaryanpagar@hotmail.com>
2022-10-17 12:37:01 +0200

    biddz checkpoint

--- custom_index.sh
+++ custom_index.sh
@@ -2,4 +2,4 @@
 
  sed -i '' 's~SIB EMAILER~'"REGEX"'~g' ./index.html
 
-sed -i '' 's|<span id="unlockedCount">\([0-9]\{0,\}\)</span>|<span id="unlockedCount">'"${unlockedCount}"'</span>|g' ./index.html
+sed -i '' 's|</head><body>|</head><body style="background-color: #131219">|g' ./biddz-index.html
