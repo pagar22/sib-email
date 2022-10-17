48d70d0e69523aebb73829600a9ca48e984f8621
pagar22 <aaryanpagar@hotmail.com>
2022-10-17 12:22:29 +0200

    checkpoint

--- custom_index.sh
+++ custom_index.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
  sed -i '' 's~SIB EMAILER~'"REGEX"'~g' ./index.html
+
+sed -i '' 's|<span id="unlockedCount">\([0-9]\{0,\}\)</span>|<span id="unlockedCount">'"${unlockedCount}"'</span>|g' ./index.html
