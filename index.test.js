b7373948536b40b7db276f85b1d33e0d4c05fc95
Aaryan Pagar <77506813+pagar22@users.noreply.github.com>
2022-12-09 09:19:21 +0100

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,3 +1,30 @@
-test('Test test to test if the test passes the test', () => {
-  expect(1 + 1).toBe(2);
-})
+import { test, expect } from "@playwright/test";
+import { login, initialiseBankAccount, waitForMe } from "../../utils";
+
+test("Buy Auction Biddz Test @e2e @structural @slow", async ({ page }) => {
+  await login(page);
+  await initialiseBankAccount(page);
+  // Go to /deals/${process.env.TEST_AUCTION_ID}
+  await page.goto(`/deals/${process.env.TEST_AUCTION_ID}`);
+  // Delay Playwright
+  await waitForMe(2000);
+  // Click div[role="button"]:has-text("biddz kaufen")
+  await page.locator('div[role="button"]:has-text("biddz kaufen")').click();
+  await expect(page).toHaveURL(
+    `/deals/${process.env.TEST_AUCTION_ID}/orders/create`
+  );
+  // Click div[role="button"]:has-text("Kaufvorschau")
+  await page.locator('div[role="button"]:has-text("Kaufvorschau")').click();
+  // Click text=ABN AMRO Bank, Frankfurt Branch****0524󰆴
+  await page.locator("text=ABN AMRO Bank, Frankfurt Branch****0524󰆴").click();
+  // Click svg[role="img"] >> nth=0
+  await page.locator('svg[role="img"]').first().click();
+  // Click svg[role="img"] >> nth=1
+  await page.locator('svg[role="img"]').nth(1).click();
+  await page.locator('div[role="button"]:has-text("Jetzt kaufen")').click();
+  // Click div[role="button"]:has-text("Meine Karten")
+  await page
+    .locator('div[role="button"]:has-text("Meine Karten")')
+    .click({ timeout: 120000 });
+  await expect(page).toHaveURL(`/lounge`);
+});
